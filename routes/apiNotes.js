cda1d72a4d9c50cd3f4ec622f003e84afbf0c3ab
Mike Sasiela <m5455y@gmail.com>
2021-02-01 20:47:45 -0600

    added post route

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -14,8 +14,34 @@ module.exports = (app) => {
             console.log(data)
             data = JSON.parse(data)
             res.json(data);
-            
         });
     })
+
+    app.post('/api/notes', (req, res) => {
+        let note = req.body
+        console.log(note)
+
+        fs.readFile(path.join(__dirname + '/../db/db.json'), 'utf8', (error, data) => {
+            if (error) throw error
+            console.log(data)
+            data = JSON.parse(data)
+            data.push(note)
+
+            fs.writeFile(path.join(__dirname + '/../db/db.json'), JSON.stringify(data), (error) => {
+                if (error) throw error
+                res.json(true);   
+            });
+        });
+    })
+
+    // app.delete('/api/notes', (req, res) => {
+    //     fs.remove(path.join(__dirname + '/../db/db.json'), 'utf8', (error, data) => {
+    //         if (error) throw error
+    //         console.log(data)
+    //         data = JSON.parse(data)
+    //         res.json(data);
+            
+    //     });
+    // })
 
 };
