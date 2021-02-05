bd922826e89ccf05413a4a7dab12328f0b725276
Mike Sasiela <m5455y@gmail.com>
2021-02-04 22:49:43 -0600

    final working product

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -1,17 +1,12 @@
 const fs = require('fs')
 const path = require('path')
+const { v4: uuidv4 } = require('uuid')
 
 module.exports = (app) => {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
 
     app.get('/api/notes', (req, res) => {
         fs.readFile(path.join(__dirname + '/../db/db.json'), 'utf8', (error, data) => {
             if (error) throw error
-            console.log(data)
             data = JSON.parse(data)
             res.json(data);
         });
@@ -19,6 +14,7 @@ module.exports = (app) => {
 
     app.post('/api/notes', (req, res) => {
         let note = req.body
+        note.id = uuidv4()
         console.log(note)
 
         fs.readFile(path.join(__dirname + '/../db/db.json'), 'utf8', (error, data) => {
@@ -29,19 +25,29 @@ module.exports = (app) => {
 
             fs.writeFile(path.join(__dirname + '/../db/db.json'), JSON.stringify(data), (error) => {
                 if (error) throw error
-                res.json(true);   
+                res.json(true);
             });
         });
     })
 
-    // app.delete('/api/notes', (req, res) => {
-    //     fs.remove(path.join(__dirname + '/../db/db.json'), 'utf8', (error, data) => {
-    //         if (error) throw error
-    //         console.log(data)
-    //         data = JSON.parse(data)
-    //         res.json(data);
+    app.delete('/api/notes/:id', (req, res) => {
+        console.log(req.params.id)
+        let note = req.body
+        console.log(note)
+        fs.readFile(path.join(__dirname + '/../db/db.json'), 'utf8', (error, data) => {
+            if (error) throw error
+            data = JSON.parse(data)
+            let filtered = data.filter(function (note) {
+            return note.id != req.params.id
+                
+            })
+            newData = filtered
+            console.log(data)
             
-    //     });
-    // })
-
+            fs.writeFile(path.join(__dirname + '/../db/db.json'), JSON.stringify(newData), (error) => {
+                if (error) throw error
+                res.json(true);
+            });
+        });
+    })
 };
